f7abeda269a31da316e5f7cc7475f0a458990443
gouthamf22labs <goutham.s@f22labs.com>
2025-03-21 14:16:02 +0530

    chore fixed to parellel container url

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 (async () => {
     const https = require('https');
 
-    const url = 'https://adsinspire-puppeteer.leiusn.easypanel.host/facebook/createsession?isIncognito=true&userAgentPos=0&page_id=103032366405927&adsLibraryUrl=https://www.facebook.com/ads/library/?active_status=active%26ad_type=all%26country=ALL%26media_type=all%26search_type=page%26view_all_page_id=103032366405927&close_browser=true'; // Replace with your API endpoint
+    const url = 'https://madangles-puppeteer.leiusn.easypanel.host/facebook/createsession?isIncognito=true&userAgentPos=0&page_id=103032366405927&adsLibraryUrl=https://www.facebook.com/ads/library/?active_status=active%26ad_type=all%26country=ALL%26media_type=all%26search_type=page%26view_all_page_id=103032366405927&close_browser=true'; // Replace with your API endpoint
 
     try {
         https.get(url, (res) => {
